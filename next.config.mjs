ea284f786aeeca8f0fb11eccd40a421d7d981423
kallarari <kallarari@gmail.com.br>
2024-08-27 10:44:34 -0300

    test(build

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import withPWAInit from "next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-});
+});/* 
 export default withPWA({
     reactStrictMode:true,
     disable: process.env.NODE_ENV === "development",
@@ -11,6 +11,7 @@ export default withPWA({
     skipWaiting: true,
     compiler:{
       ssr:true,
-      displayName:true
+      displayName:true,
+      
     }
-})
+}) */
